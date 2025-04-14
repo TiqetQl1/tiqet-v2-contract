0a0ba1bcc2bea3278fc6e0d14af81887a3c71830
Kian Arabi <k80arabi@outlook.com>
2025-04-14 17:03:50 +0330

    Shortcut script to ignite

--- ignition/modules/bootstrap.ts
+++ ignition/modules/bootstrap.ts
@@ -2,7 +2,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules"
 import TiQetCoinModule from "./TiQetCoinModule";
 import TiQetNFTModule from "./TiQetNFTModule";
 
-export default buildModule("Bootstrap chain", (m) => {
+export default buildModule("BootstrapChain", (m) => {
     const { tiqetCoin } = m.useModule(TiQetCoinModule);
     const { tiqetNFT }  = m.useModule(TiQetNFTModule);
   
