b4853f81ca59200e2987cc2e2648cb80237e2f7f
Kian Arabi <k80arabi@outlook.com>
2025-04-15 14:54:26 +0330

    Creates test cases of the accessControl contract

---
+++ test/authTests.ts
@@ -0,0 +1,75 @@
+import { expect } from "chai";
+import hre from "hardhat";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { AccessControl } from "../typechain-types";
+import { ContractTransactionResponse } from "ethers";
+
+describe('Auth', () => {
+    let accessControl : (AccessControl & { deploymentTransaction(): ContractTransactionResponse });
+    let accounts : HardhatEthersSigner[];
+    let owner: HardhatEthersSigner;
+
+    const deployFixture = async () => {
+        const _accounts = await hre.ethers.getSigners()
+        return {_accounts}
+    }
+    beforeEach(async () => {
+        const {_accounts} = await loadFixture(deployFixture)
+
+        accounts  =_accounts
+        owner     = accounts[0]
+    });
+
+    it("Owner is set correctly",async ()=>{ 
+        //TODO...
+    })
+
+    it("Owner can change ownership",async ()=>{ 
+        //TODO...
+    })
+
+    it("CRUD on admins",async ()=>{ 
+        //TODO...
+    })
+
+    it("CRUD on proposers",async ()=>{ 
+        //TODO...
+    })
+
+    it("CRUD on credible NFT collections",async ()=>{ 
+        //TODO...
+    })
+
+    it("is_owner",async ()=>{ 
+        //TODO...
+    })
+
+    it("is_admin",async ()=>{ 
+        //TODO...
+    })
+
+    it("is_proposer",async ()=>{ 
+        //TODO...
+    })
+
+    it("has_nft",async ()=>{ 
+        //TODO...
+    })
+
+    it("modifier: eq_owner", async ()=>{
+        //TODO...
+    })
+
+    it("modifier: gteq_admin",async ()=>{ 
+        //TODO...
+    })
+
+    it("modifier: gteq_proposer",async ()=>{ 
+        //TODO...
+    })
+
+    it("modifier: gteq_holder",async ()=>{ 
+        //TODO...
+    })
+})
