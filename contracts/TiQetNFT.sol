ac395208e61363de2d3e93ad81270f757b8586b9
Kian Arabi <k80arabi@outlook.com>
2025-04-16 14:33:37 +0330

    Changes the token to a mintable one

--- contracts/TiQetNFT.sol
+++ contracts/TiQetNFT.sol
@@ -2,7 +2,34 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TiQetNFT is ERC721 {
-    constructor() ERC721("TiQetNFT", "TQFT") {}
+contract TiQetNFT is ERC721, ERC721Pausable, Ownable {
+    constructor(address initialOwner)
+        ERC721("TiQetNFT", "TQFT")
+        Ownable(initialOwner)
+    {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Pausable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
 }
