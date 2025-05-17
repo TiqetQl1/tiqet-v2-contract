651ef2424ae2a4bdca0b35fd7fe2ca8cbb008d71
Kian Arabi <k80arabi@outlook.com>
2025-05-17 15:52:18 +0330

    Adds optimization

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 import PRIVATE_KEYS from "./ignition/accounts";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
