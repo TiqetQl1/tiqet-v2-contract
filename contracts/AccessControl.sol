295974d66c42ffffdb2ba953b90d7e7639aaf305
Kian Arabi <k80arabi@outlook.com>
2025-06-06 21:40:19 +0330

    publishes auth_is_X functions

--- contracts/AccessControl.sol
+++ contracts/AccessControl.sol
@@ -149,28 +149,28 @@ contract AccessControl {
     /// @notice Will be used in modifiers
     /// @param check The address to be checked
     /// @return bool true if `check` is owner, and false otherwise
-    function auth_is_owner(address check) internal view returns(bool){
+    function auth_is_owner(address check) public view returns(bool){
         return _owner == check;
     }
 
     /// @notice Will be used in modifiers
     /// @param check The address to be checked
     /// @return bool true if `check` is admin, and false otherwise
-    function auth_is_admin(address check) internal view returns(bool){
+    function auth_is_admin(address check) public view returns(bool){
         return array_find(check, _admins)!=NOT_FOUND;
     }
 
     /// @notice Will be used in modifiers
     /// @param check The address to be checked
     /// @return bool true if `check` is proposer, and false otherwise
-    function auth_is_proposer(address check) internal view returns(bool){
+    function auth_is_proposer(address check) public view returns(bool){
         return array_find(check, _proposers)!=NOT_FOUND;
     }
 
     /// @notice Will be used in modifiers
     /// @param check The address to be checked
     /// @return bool true if `check` has nft, and false otherwise
-    function auth_is_nftholder(address check) internal view returns(bool){
+    function auth_is_nftholder(address check) public view returns(bool){
         uint256 len = _nftList.length;
         for (uint256 i = 0; i < len; i++) {
             ERC721 nft_contract = ERC721(_nftList[i]);
