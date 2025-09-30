2e3d17dd239b8570df1d930f857ecefeb04489eb
KAYT33N <k80arabi@outlook.com>
2025-09-30 16:37:32 +0330

    Seperates qusdt token contract from the tiqet token

--- ignition/modules/contracts/qusdt.ts
+++ ignition/modules/contracts/qusdt.ts
@@ -5,7 +5,7 @@ import PRIVATE_KEYS from "../../accounts";
 export default buildModule("qusdt", (m) => {
     const signers = PRIVATE_KEYS.map(key=>new Wallet(key))
     const owner   = signers[0]
-    const qusdt = m.contract("TestERC20Token", [owner.address]);
+    const qusdt = m.contract("TestERC20QUSDT", [owner.address]);
 
     return { qusdt };
 });
